e480f3850158dfbb3d757f64b5346b07a794dd96
Opt1mus26 <pab1o@mail.ru>
2022-12-28 13:24:43 +0300

    new style by bura

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { TodoListItem } from "./todo-list-item";
-import './todo-list.css'
+import React from 'react';
 
-const TodoList = ({todos}) => {
+import TodoListItem from './todo-list-item';
+import './todo-list.css';
 
-const elements = todos.map((item) => {
+const TodoList = ({ todos }) => {
 
-const { id, ...itemProps } = item;
-
-  return (
-  <li key={id} className="list-group-item">
-    <TodoListItem { ...itemProps } />
-  </li>
-  );
-});
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
 
     return (
-      <ul className="list-group todo-list">
-       { elements }
-      </ul>
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
     );
-  };
+  });
 
-  export default TodoList;
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
+};
+
+export default TodoList;
